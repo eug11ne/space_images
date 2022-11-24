2f314ea66eaec86c7e645263c077b73cf6f4409c
eug11ne <114510061+eug11ne@users.noreply.github.com>
2022-11-24 18:53:42 +0300

    Publish photo to Telegram channel.

--- tg-bot.py
+++ tg-bot.py
@@ -1,12 +1,24 @@
 import os
 import telegram
 from dotenv import load_dotenv
+from pathlib import Path
+import random
 
 load_dotenv()
 tg_key = os.getenv('TG_KEY')
-print(tg_key)
 bot = telegram.Bot(token=tg_key)
-print(bot.get_me())
-chat_id='@space_overview'
-bot.send_message(chat_id=chat_id, text="You are about to explore the universe")
+chat_id = '@space_overview'
+
+image_paths = [img_path for img_path in Path('images').glob('*.*')]
+image = random.choice(image_paths)
+
+with open(image, 'rb') as image_file:
+    size = image_file.seek(0, os.SEEK_END)
+    image_file.seek(0)
+    print(size)
+    if size < 20000000:
+        bot.send_photo(chat_id=chat_id, photo=image_file)
+        print("Random photo published to tg channel.")
+    else:
+        print("File is too big.")
 
