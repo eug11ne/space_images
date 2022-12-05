6d706397c1a65998bc68348d5490429b1ad5f9df
eug11ne <114510061+eug11ne@users.noreply.github.com>
2022-12-05 20:19:04 +0300

    Update download_tools.py

--- download_tools.py
+++ download_tools.py
@@ -20,19 +20,3 @@ def get_image(url, path, token=0):
 
 
 
-'''
-
-def get_image(url, path):
-    a = urlparse(url)
-    file_name = Path(a.path).name
-    Path(path).mkdir(parents=True, exist_ok=True)
-    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)'}
-    img = requests.get(url, headers=headers)
-    img.raise_for_status()
-    try:
-        with open(f'images/{file_name}', 'wb') as file:
-            file.write(img.content)
-        return True
-    except requests.exceptions.HTTPError:
-        return False
-'''
